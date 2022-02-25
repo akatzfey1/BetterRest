57ac8b9ef39cb78a65260aa3e13e6431efa7bbb1
akatzfey <katzfeyalex@gmail.com>
2022-02-25 13:25:25 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alexander Katzfey on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
